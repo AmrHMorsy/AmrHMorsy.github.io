99e1826e0210c56618f4783228d678338aff2b00
AmrHMorsy <AmrMHMorsy@gmail.com>
2026-07-01 08:02:00 +0000

    Deploying to gh-pages from @ AmrHMorsy/AmrHMorsy.github.io@ac8cad421aca54a5ad4aaa8c711d4f31d6881c6c 🚀

---
+++ Push.sh
@@ -0,0 +1,7 @@
+rm -rf .git
+git init
+git add .
+git commit -m “First Commit”
+git remote add origin https://github.com/amrhmorsy/amrhmorsy.github.io
+git branch -m "main"
+git push origin main --force
